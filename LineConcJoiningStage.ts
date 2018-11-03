f7f1b7c04bee36a2037107b2b74af4a8ad2522df
Anwesh43 <anweshthecool0@gmail.com>
2018-11-03 17:08:33 +0530

    adding LineConcJoining class

--- LineConcJoiningStage.ts
+++ LineConcJoiningStage.ts
@@ -153,3 +153,25 @@ class LCJNode {
         return this
     }
 }
+
+class LineConcJoining {
+    curr : LCJNode = new LCJNode(0)
+    dir : number = 1
+    
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
